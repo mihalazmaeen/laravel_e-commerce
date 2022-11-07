6f0964ef3be17c9e5b5be7357f75114183f084ee
mihalazmaeen <azmaeenmihal1@gmail.com>
2022-11-07 15:51:50 +0600

    second

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -4,19 +4,29 @@
     <div class="d-flex align-items-center justify-content-center bg-sl-primary ht-100v">
 
         <div class="login-wrapper wd-300 wd-xs-350 pd-25 pd-xs-40 bg-white">
-            <div class="signin-logo tx-center tx-24 tx-bold tx-inverse">starlight <span class="tx-info tx-normal">admin</span></div>
-            <div class="tx-center mg-b-60">Professional Admin Template Design</div>
-
+            <div class="signin-logo tx-center tx-24 tx-bold tx-inverse">Login <span class="tx-info tx-normal">admin</span></div>
+<form method="post" action="{{ route('admin.login') }}">
+    @csrf
             <div class="form-group">
-                <input type="text" class="form-control" placeholder="Enter your username">
+                <input type="text" class="form-control" name="email" placeholder="Enter your Email">
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
             </div><!-- form-group -->
             <div class="form-group">
-                <input type="password" class="form-control" placeholder="Enter your password">
+                <input type="password" name="password" class="form-control" placeholder="Enter your password" />
+                @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
                 <a href="" class="tx-info tx-12 d-block mg-t-10">Forgot password?</a>
             </div><!-- form-group -->
-            <button type="submit" class="btn btn-info btn-block">Sign In</button>
+            <button type="submit" class="btn btn-info btn-block">Log In</button>
+</form>
 
-            <div class="mg-t-60 tx-center">Not yet a member? <a href="page-signup.html" class="tx-info">Sign Up</a></div>
         </div><!-- login-wrapper -->
     </div><!-- d-flex -->
 @endsection
